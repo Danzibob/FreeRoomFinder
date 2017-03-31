d85d61a1889d91f5d6c62073ae90fa4028a91364
Danny Roberts <danzibob@gmail.com>
2017-03-31 19:32:08 +0100

    Added time based search functionality

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
-var A = WEEKA
-console.log(A)
+var week = {
+	"A":WEEKA,
+	"B":WEEKB
+}
+$().ready(function(){
+
+	$("#header").click(function(){
+		console.log("Clicked!")
+		if($('#MainMenu').css('display') == "none"){
+			$(".box").slideUp(400,function(){
+				$("#MainMenu").fadeIn(400);
+				$("#MainMenu").children().fadeIn(400);
+			})
+		}
+	})
+
+	$("#TimeSearch .submit").click(function(){
+		$("#TimeSearch .output p").remove();
+		var query = $( "#day" ).val() + ":" + $( "#period" ).val()
+		var results = week[$( "#week" ).val()][query]
+		results = results.join(", ")
+		$("#TimeSearch .output").append("<p>"+ results +"</p>")
+	})
+
+	$("#timeMode").click(function(){
+		$("#MainMenu").slideUp(400,function(){
+			$("#TimeSearch").fadeIn(400)
+		})
+	})
+})
