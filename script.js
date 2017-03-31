9fc8b16a31fb8cc3c5c6b2092bfe25464f9a2ca8
Danny Roberts <danzibob@gmail.com>
2017-03-31 20:03:41 +0100

    Added room search funcitonality and hid adv search

--- script.js
+++ script.js
@@ -7,15 +7,17 @@ $().ready(function(){
 	$("#header").click(function(){
 		console.log("Clicked!")
 		if($('#MainMenu').css('display') == "none"){
-			$(".box").slideUp(400,function(){
-				$("#MainMenu").fadeIn(400);
-				$("#MainMenu").children().fadeIn(400);
-			})
+			$(".box").slideUp(400)
+			function fadein(){
+				$("#MainMenu").children().show();
+				$("#MainMenu").fadeIn();
+			}
+			setTimeout(fadein,500)
 		}
 	})
 
 	$("#TimeSearch .submit").click(function(){
-		$("#TimeSearch .output p").remove();
+		$("#TimeSearch .output p").remove()
 		var query = $( "#day" ).val() + ":" + $( "#period" ).val()
 		var results = week[$( "#week" ).val()][query]
 		results = results.join(", ")
@@ -27,4 +29,29 @@ $().ready(function(){
 			$("#TimeSearch").fadeIn(400)
 		})
 	})
+
+	$("#roomMode").click(function(){
+		$("#MainMenu").slideUp(400,function(){
+			$("#RoomSearch").fadeIn(400)
+		})
+	})
+
+	$("#RoomSearch .submit").click(function(){
+		$("#RoomSearch .output p, h3").remove()
+		var query = $( "#roomCode" ).val()
+		for(var w in week){
+			var results = []
+			for(var i in week[w]){
+				if(week[w][i].indexOf(query) > -1){
+					results.push(i)
+				} 
+			}
+			console.log(results)
+			results = results.join(", ")
+			$("#RoomSearch .output").append("<h3>"+ "Week " + w +"</h3>")
+			$("#RoomSearch .output").append("<p>"+ results +"</p>")
+		}
+
+	})
 })
+
